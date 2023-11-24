3b7e9e426ff012b4b8311a5858dc87b63c539a44
mdzslp <150118879+mdzslp@users.noreply.github.com>
2023-11-24 09:24:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,44 @@
 let addMessage = document.querySelector(".message");
 let addButton = document.querySelector(".add");
 let todo = document.querySelector(".todo");
+let delElement = document.querySelector(".delete");
+// let editElement = document.querySelector(".edit");
 
-let todoList = [];
-
-if (localStorage.getItem("todo")) {
-  //   todoList = JSON.parse(localStorage.getItem("todo"));
-  displayMessages();
-}
+let todoList = []; // массив данных списка
 
 addButton.addEventListener("click", function () {
+  // Добавление задач в список по кнопке
+  if (!addMessage.value) return alert("Введите текст!!!"); // Ограничение пустого ввода
   let newTodo = {
     todo: addMessage.value,
     checked: false,
     important: false,
   };
-
   todoList.push(newTodo);
+
   displayMessages();
-  //   localStorage.setItem("todo", JSON.stringify(todoList));
+  addMessage.value = "";
 });
 
 function displayMessages() {
   let displayMessage = "";
+  if (todoList.length === 0) todo.innerHTML = "";
   todoList.forEach(function (item, i) {
-    displayMessage += `
+    // Формирование списка
+    displayMessage += `  
     <li>
-        <input type = 'checkbox' id='item_${i}' ${
-      item.checked ? "checked" : ""
-    }>
-        <label for = 'item_${i}'> ${item.todo} </label>
+     <input type="checkbox" id="item_${i}" ${item.checked ? "checked" : ""}>
+     <label for="item_${i}">${item.todo}</label>
     </li>
     `;
     todo.innerHTML = displayMessage;
   });
 }
+
+delElement.addEventListener("click", function () {
+  // Удаление данных из списка
+  todoList.forEach(function (item) {
+    todoList.splice(item);
+  });
+  displayMessages();
+});
